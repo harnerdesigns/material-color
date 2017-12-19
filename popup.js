160d7003032d664cdc9b96a962a2f317abc2d2c7
Jack Harner <jack@harnerdesigns.com>
2017-12-19 15:33:55 -0700

    got the color variations working

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ var $color = (function() {
         $.ajax({
             'async': false,
             'global': false,
+            'cache': false,
             'url': "js/color.json",
             'dataType': "json",
             'success': function (data) {
@@ -16,11 +17,30 @@ var $color = (function() {
 
 console.log($color);
 
+$.each($color.alternateNames, function(key, data) { 
+    if(this == "A100"){$('.sideBar').append("<hr>")};
+    $('.sideBar').append(
+        '<div class="color">'
+        + this + '</div>'
+    );
+
+});
+
     $.each($color.mainColors, function(key, data) {
       console.log(this.hex + key);
     $('.mainColors').append(
         '<div class="color" style="background:'+ this.hex +'" data-hex="' + this.hex + '" id="'+ key +'">'
         + this.name + '</div>'
     );
+
+    $('.variations').append("<div class='colorset' id='colorset-" + key + "'></div>" );
+
+    $.each(this.alternates, function(keys, data){
+        if(keys === "A100"){$('#colorset-' + key).append("<hr>")};
+        $('#colorset-' + key).append(
+        '<div class="color" style="background:'+ this +'" data-hex="' + this + '" id="'+ keys +'"></div>'
+    );
+    });
+
+    });
 });
-  });
