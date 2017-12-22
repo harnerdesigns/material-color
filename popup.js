9f48d49fe2c8524e53b22d74a45152064906dcc7
Jack Harner <jack@harnerdesigns.com>
2017-12-22 13:18:06 -0700

    Got colorOutput to work

--- popup.js
+++ popup.js
@@ -1,151 +1,164 @@
-$( document ).ready( function(){
+// Get Color Object from color.json
 
-var $color = (function() {
-        var $color = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'cache': false,
-            'url': "js/color.json",
-            'dataType': "json",
-            'success': function (data) {
-                $color = data;
-            }
+        var $color = (function() {
+            var $color = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'cache': false,
+                'url': "js/color.json",
+                'dataType': "json",
+                'success': function(data) {
+                    $color = data;
+                }
+            });
+            return $color;
+        })();
+
+// Get Output Setting from Chrome.Storage and send it to add_colors()
+        $(document).ready(function() {
+
+           get_output_setting(add_colors);
+
         });
-        return $color;
-    })();
 
-    $("header").css("background", $color.mainColors.red.hex);
+// Get colorOutput and call callback function
+        function get_output_setting(callback) {
+            var storageItems = chrome.storage.sync.get("colorOutput", function(items) {
+                callback(items.colorOutput);
+            });
+        }
 
-$.each($color.alternateNames, function(key, data) { 
-    if(this == "A100"){$('.sideBar').append("<hr>")};
-    $('.sideBar').append(
-        '<div class="square">'
-        + this + '</div>'
-    );
-
-});
-
-    $.each($color.mainColors, function(key, data) {
-    $('.mainColors').append(
-        '<div class="color" data-clipboard-text="'+ color(this.hex) + '" style="background:'+ this.hex +'" data-hex="' + this.hex + '" id="'+ key +'">'
-        + this.name + '</div>'
-    );
-
-    $('.variations').append("<div class='colorset' id='colorset-" + key + "'></div>" );
-
-    $.each(this.alternates, function(keys, data){
-        if(keys === "A100"){$('#colorset-' + key).append("<hr>")};
-        $('#colorset-' + key).append(
-        '<div class="color" data-clipboard-text="'+ this + '" style="background:'+ this +'" title="' + this +'" data-hex="' + this + '" id="'+ keys +'"></div>'
-    );
-    });
-
-    });
-
-
-    $(".color").click(function(){
-        $("header").css("background", $(this).data("hex"));
-        if ($(this).attr('id') === "a50"){
-            $("header").css("color", "#000");
-        } else {$("header").css("color", "#fff");}
+// Generate the colors and selection based on output setting
+        function add_colors(output) {
         
-        $("#headerAlert").stop(true, true);
-        $("#headerAlert").fadeIn();
-        $("#headerAlert").html("Copied to Clipboard: " + $(this).data("clipboard-text"));
-        $('#headerAlert').delay(1000).fadeOut();
-    });
+            $.each($color.mainColors, function(key, data) {
+                $('.mainColors').append(
+                    '<div class="color" data-clipboard-text="' + color(this.hex, output) + '" style="background:' + this.hex + '" data-hex="' + this.hex + '" id="' + key + '">' +
+                    this.name + '</div>'
+                );
 
-new Clipboard('.color');
+                $('.variations').append("<div class='colorset' id='colorset-" + key + "'></div>");
 
+                $.each(this.alternates, function(keys, data) {
+                    if (keys === "A100") {
+                        $('#colorset-' + key).append("<hr>")
+                    };
+                    $('#colorset-' + key).append(
+                        '<div class="color" data-clipboard-text="' + color(this, output) + '" style="background:' + this + '" title="' + this + '" data-hex="' + this + '" id="' + keys + '"></div>'
+                    );
+                });
 
-});
+            });
+            // Colorize header
+            $("header").css("background", $color.mainColors.red.hex);
 
+            // Add Sidebar
+            $.each($color.alternateNames, function(key, data) {
+                if (this == "A100") {
+                    $('.sideBar').append("<hr>")
+                };
+                $('.sideBar').append(
+                    '<div class="square">' +
+                    this + '</div>'
+                );
 
-// function color(hex) {
+            });
 
-//     var output;
-//     var convertedHex;
+            // Bind on click
+            $(".color").click(function() {
+                $("header").css("background", $(this).data("hex"));
+                if ($(this).attr('id') === "a50") {
+                    $("header").css("color", "#000");
+                } else {
+                    $("header").css("color", "#fff");
+                }
 
-//     chrome.storage.sync.get("colorOutput", function(items) {
-//         if (!chrome.runtime.error) {
-//             var output = items.colorOutput;
+                // Alert color has been copied
+                $("#headerAlert").stop(true, true);
+                $("#headerAlert").fadeIn();
+                $("#headerAlert").html("Copied to Clipboard: " + $(this).data("clipboard-text"));
+                $('#headerAlert').delay(1000).fadeOut();
+            });
 
-//         }
-
-//         if (output === "rgb") {
-
-//             console.log("rgb");
-
-//         }
-//         if (output === "hex") {
-//             console.log("hex___");
-//             convertedHex = hex.replace(/([#])/g, "");
-//             console.log(convertedHex);
-//         }
-//         if (output === "rgba") {
-//             console.log("rgba");
-
-//         }
-//         if (output === "hashHex") {
-//             console.log("hashHex");
-//             hex = hex;
-//         }
-
-//         return convertedHex;
-//     });
-function get_output_setting(){
-var storageItems = chrome.storage.sync.get("colorOutput", function(items){return items.colorOutput});
-}
-
-
-function color(hex) {
-
-    var output;
-    var convertedHex;
-
-    //get colorOutput items from local storage
-    var storageItems = chrome.storage.sync.get("colorOutput", function(items){
-    if (!chrome.runtime.error) {
-        //convert the items
-         console.log("Success!");
-         console.log(items);
-     convert_hex_to_rgb(hex);
-    }
-    });
-    console.log(storageItems);
-
-    
-    //check for runtime error
-
-}
-
-function convert_hex_to_rgb(hex){
-           console.log("converted hex: " + hex + " to rgb");
-            convertedHex = hex;
-        return convertedHex;
-    }
-
- function convert_hex_to_rgba(hex){
-        console.log(items);
-        if (items === "rgb") {
-
-            console.log("converted hex to rgba");
-            convertedHex = hex;
+            new Clipboard('.color');
 
         }
-        return convertedHex;
-    }
 
-    function convert_hex_to_hex(hex){
-        console.log(items);
-        if (items === "rgb") {
 
-            console.log("hex___");
-            convertedHex = hex.replace(/([#])/g, "");
-            console.log(convertedHex);
+        // Determine how to output and then output
+        function color(hex, output) {
 
+
+            switch (output) {
+                case "hashHex":
+                    console.log("Output is HashHex");
+                    convertedHex = hex;
+                    break;
+                case "hex":
+                    console.log("Output is hex");
+                    convertedHex = convert_hex_to_hex(hex);
+                    break;
+                case "rgb":
+                    console.log("Output is rgb");
+                    convertedHex = convert_hex_to_rgb(hex);
+                    break;
+                case "rgba":
+                    console.log("Output is rgba");
+                    convertedHex = convert_hex_to_rgba(hex);
+                    break;
+                default:
+                    convertedHex = hex;
+                    break;
+            }
+
+            return convertedHex;
         }
-        return convertedHex;
-    }
 
+
+
+
+        function convert_hex_to_rgb(hex) {
+
+            R = hexToR(hex);
+            G = hexToG(hex);
+            B = hexToB(hex);
+
+            var rgb = "rgb(";
+                rgb += R; 
+                rgb += ","; 
+                rgb += G; 
+                rgb += ","; 
+                rgb += B; 
+                rgb += ")"; 
+            return rgb;
+        }
+
+        function convert_hex_to_rgba(hex) {
+            R = hexToR(hex);
+            G = hexToG(hex);
+            B = hexToB(hex);
+            A = "1"
+
+            var rgba = "rgba(";
+                rgba += R; 
+                rgba += ","; 
+                rgba += G; 
+                rgba += ","; 
+                rgba += B; 
+                rgba += ","; 
+                rgba += A; 
+                rgba += ")"; 
+            return rgba;
+        }
+
+        function convert_hex_to_hex(hex) {
+                hex = hex.replace(/([#])/g, "");
+                return hex;
+
+            }
+
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
